4faf87bedca2601ff921d707b0674732405220ea
ZzyzZzZz <17434@students.mrgs.school.nz>
2021-05-31 15:18:38 +1200

    I just realised using an listView array with String will work just fine, since the values in integer is displaying just fine. So I will stop trying the hashmap method. Oh well. I have already made the part that turn int to String, I should have known.

--- app/src/main/java/com/zhu/a13cscproject/OrderConfirm.java
+++ app/src/main/java/com/zhu/a13cscproject/OrderConfirm.java
@@ -12,12 +12,9 @@ import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
 
 public class OrderConfirm extends AppCompatActivity {
 
-    TextView textView1, textView2, textView3;
     ListView order_confirm_list;
 
     @RequiresApi(api = Build.VERSION_CODES.N)
@@ -52,27 +49,33 @@ public class OrderConfirm extends AppCompatActivity {
 
         order_confirm_list = findViewById(R.id.order_confirm_list); // this is the creation of a list view,
         // where I list everything in order and exclude things that aren't chosen.
-        ArrayList<Integer> order_confirm_array = new ArrayList<>();
+        ArrayList<String> order_confirm_array = new ArrayList<>(); //I turned int to String so I can display both String and Int.
 
         if (chips_qty_num != 0){
-            order_confirm_array.add(chips_qty_num);
+            order_confirm_array.add("chips" + chips_qty_num);
         }
-        ArrayAdapter order_confirm_list_adaptor = new ArrayAdapter(this, android.R.layout.simple_list_item_1, order_confirm_array);
+        if (hotdog_qty_num != 0){
+            order_confirm_array.add("hotdog" + kebab_qty_num);
+        }
+        if (kebab_qty_num != 0){
+            order_confirm_array.add("kebab" + kebab_qty_num);
+        }
+        ArrayAdapter<String> order_confirm_list_adaptor = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, order_confirm_array);
 
         order_confirm_list.setAdapter(order_confirm_list_adaptor);
 
-        // hashmap
-        Map<String, Integer> numberMapping = new HashMap<>();
-
-        // Adding key-value pairs to a HashMap
-        numberMapping.put("CHIPS", chips_qty_num);
-        numberMapping.put("HOTDOG", hotdog_qty_num);
-        numberMapping.put("KEBAB", kebab_qty_num);
-
-        // Add a new key-value pair only if the key does not exist in the HashMap, or is mapped to `null`
-        //numberMapping.putIfAbsent("Four", 4);
-
-        System.out.println(numberMapping);
+//        // hashmap
+//        Map<String, Integer> numberMapping = new HashMap<>();
+//
+//        // Adding key-value pairs to a HashMap
+//        numberMapping.put("CHIPS", chips_qty_num);
+//        numberMapping.put("HOTDOG", hotdog_qty_num);
+//        numberMapping.put("KEBAB", kebab_qty_num);
+//
+//        // Add a new key-value pair only if the key does not exist in the HashMap, or is mapped to `null`
+//        //numberMapping.putIfAbsent("Four", 4);
+//
+//        System.out.println(numberMapping);
 
 
 
